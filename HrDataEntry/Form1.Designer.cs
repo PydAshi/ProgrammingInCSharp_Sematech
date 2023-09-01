3671e86ee6869dbb34d49ad736f77887e86352a6
PydAshi <142756137+PydAshi@users.noreply.github.com>
2023-09-01 17:21:55 +0330

    Add project files.

---
+++ HrDataEntry/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace HrDataEntry
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSave = new Button();
+            lblFirstName = new Label();
+            txtFirstName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(249, 71);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 0;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Location = new Point(110, 19);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(76, 20);
+            lblFirstName.TabIndex = 1;
+            lblFirstName.Text = "FirstName";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(227, 12);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(125, 27);
+            txtFirstName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtFirstName);
+            Controls.Add(lblFirstName);
+            Controls.Add(btnSave);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSave;
+        private Label lblFirstName;
+        private TextBox txtFirstName;
+    }
+}
---
+++ HrDataEntry/Form1.cs
@@ -0,0 +1,15 @@
+namespace HrDataEntry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string strFName = txtFirstName.Text;
+        }
+    }
+}
